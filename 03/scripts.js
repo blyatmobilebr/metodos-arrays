550b96688d5eaf7e01b19e03b4f573e046590474
Кариша <lucaslima2458@gmail.com>
2022-11-30 16:14:31 -0300

    chore: 03 resolvido

---
+++ 03/scripts.js
@@ -0,0 +1,6 @@
+const nomes = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
+const indiceNoArray = 3;
+const resposta = `${nomes.slice(indiceNoArray, indiceNoArray + 3).join(" - ")}`;
+
+
+console.log(resposta);
