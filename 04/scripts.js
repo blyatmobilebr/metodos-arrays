f7b96f33d88dc0f3309a1118fc2eed8eab34db13
Кариша <lucaslima2458@gmail.com>
2022-11-30 16:14:36 -0300

    chore: 04 resolvido

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,1 +1,14 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+
+function fila(lista, tipoOperacao, nomePaciente) {
+    if (tipoOperacao === "agendar") { 
+        lista.push(nomePaciente);
+    }
+    else if (tipoOperacao === "atender") lista.shift();
+
+    return lista.join(", ");
+}
+
+
+console.log(fila(pacientes, "agendar", "Miau"));
