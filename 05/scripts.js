58bb8ad93b226bbe13e7d026daeb43c5547e3275
Кариша <lucaslima2458@gmail.com>
2022-11-30 16:14:42 -0300

    chore: 05 resolvido

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,1 +1,23 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+
+function agendar(lista, tipoOperacao, nomePaciente) {
+    if (tipoOperacao === "agendar") lista.push(nomePaciente);
+
+    return lista.join(", ");
+}
+
+function atender(lista, tipoOperacao, _nomePaciente) {
+    if (tipoOperacao === "atender") lista.shift();
+
+    return lista.join(", ");
+}
+
+function cancelar(lista, tipoOperacao, nomePaciente) {
+    if (tipoOperacao === "cancelar") return lista.filter(nome => nome !== nomePaciente).join(", ")
+
+}
+
+console.log(atender(pacientes, "atender"));
+console.log(agendar(pacientes, "agendar", "João"));
+console.log(cancelar(pacientes, "cancelar", "João"));
